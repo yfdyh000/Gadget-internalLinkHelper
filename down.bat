828a7eedf296ccd0a8f921bdd9a4126f80d4bef5
YFdyh000 <yfdyh000@gmail.com>
2022-04-04 23:20:07 +0800

    cleanup

--- down.bat
+++ down.bat
@@ -2,34 +2,34 @@ rem https://zh.wikipedia.org/wiki/MediaWiki:Gadgets-definition
 
 
 REM internalLinkHelper-redonly
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
 REM internalLinkHelper-redtipsy
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js
 REM internalLinkHelper-redplain
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redplain.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redplain.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redplain.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redplain.js"
 REM internalLinkHelper-external[ResourceLoader
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-external.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-external.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-external.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-external.js"
 REM internalLinkHelper-suffix[ResourceLoader
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-external.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-suffix.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-external.js"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-suffix.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-external.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-suffix.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-external.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-suffix.js"
 REM internalLinkHelper-cravix[ResourceLoader
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-cravix.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-cravix.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-cravix.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-cravix.js"
 REM internalLinkHelper-altcolor[ResourceLoader
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-altcolor.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-altcolor.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js"
 REM internalLinkHelper-ilbluehl[ResourceLoader
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-altcolor.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-ilbluehl.css"
-wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&ctype=text/css&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redonly.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-altcolor.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-ilbluehl.css"
+wget --content-disposition --directory-prefix=orig "https://zh.wikipedia.org/w/index.php?action=raw&title=MediaWiki:Gadget-internalLinkHelper-redtipsy.js"
